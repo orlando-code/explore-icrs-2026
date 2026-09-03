02090eddeb59d59f906cef25c5530255150d7f36
Orlando Timmerman <rt582@cam.ac.uk>
2026-09-03 14:47:43 +1200

    updated language of non-presenting co-author

--- js/network.js
+++ js/network.js
@@ -807,7 +807,7 @@ function profileForSpeakerName(label, speakerProfiles, profileByPersonKey) {
 function attendanceLabel(node) {
   if (node?.attended) return "attended";
   if (node?.on_programme) return "on programme, did not attend";
-  return "non-attending or private co-author";
+  return "non-presenting co-author";
 }
 
 function isExternalCoauthor(node) {
